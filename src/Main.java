aa390cde24739a97b45d1ba67f9314dc5c226023
jainharsh524 <harshnareshjain1729@gmail.com>
2025-04-18 16:54:59 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 10;
+        int b = 100;
+        System.out.println(a+b);
     }
 }
