1a5c8ec809f4e88b87e8b4fe02c4de8326fe1af7
jainharsh524 <harshnareshjain1729@gmail.com>
2025-04-28 14:28:06 +0000

    [LeetCode] Runtime - 57 ms (5.21%), Memory - 40.7 MB (99.98%)

---
+++ solutions/0278-first-bad-version/solution.java
@@ -0,0 +1,16 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low = 0;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid)&&!isBadVersion(mid-1)) return mid;
+            else if (!isBadVersion(mid)) low = mid + 1;
+            else if(isBadVersion(mid))high = mid - 1;
+        }
+    return low;
+    }
+}
