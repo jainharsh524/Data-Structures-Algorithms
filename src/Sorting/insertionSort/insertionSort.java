67679849c5384c1797fef6b4b0e4bf21c29ba84f
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-08 17:47:55 +0530

    Lecture - Sorting updated

--- src/Sorting/insertionSort/insertionSort.java
+++ src/Sorting/insertionSort/insertionSort.java
@@ -1,4 +1,20 @@
 package Sorting.insertionSort;
 
 public class insertionSort {
+    public static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    public static void sort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j >0; j--) {
+                if(arr[j]<arr[j-1]) swap(arr, j, j-1);
+                else break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+    }
 }
