5d4c50e38faaabe9ccc78577dfdc708e57b53b28
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-08 17:59:56 +0530

    Lecture - Sorting updated

--- src/Sorting/cyclicSort/cyclicSort.java
+++ src/Sorting/cyclicSort/cyclicSort.java
@@ -1,8 +1,19 @@
 package Sorting.cyclicSort;
 
 public class cyclicSort {
+    public static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
     public static void sort(int[] arr){
-
+        int i =0;
+        while(i< arr.length) {
+            if(arr[i]-1!=i){
+                swap(arr, i, arr[i]-1);
+            }
+            else i++;
+        }
     }
     public static void main(String[] args) {
 
