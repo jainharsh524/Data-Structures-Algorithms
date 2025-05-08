dd3d0ec98387357d455dec7efdfc572347013ece
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-08 17:52:07 +0000

    [LeetCode] Runtime - 1 ms (29.72%), Memory - 45.6 MB (99.92%)

---
+++ solutions/0268-missing-number/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    public static int missingNumber(int[] nums) {
+        int i =0;
+        while(i<nums.length) {
+            if(nums[i]<nums.length&&nums[i]!=i){
+                swap(nums, i, nums[i]);
+            }
+            else i++;
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j]!=j) return j;
+        }
+        return nums.length;
+    }
+}
