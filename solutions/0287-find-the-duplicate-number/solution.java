45ad45b5a6c253a9f00b237a1b6b55c7d7c7c6e1
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-09 10:58:41 +0000

    [LeetCode] Runtime - 6 ms (45.67%), Memory - 58.5 MB (99.97%)

--- solutions/0287-find-the-duplicate-number/solution.java
+++ solutions/0287-find-the-duplicate-number/solution.java
@@ -1,17 +1,21 @@
 public class Solution {
-    public int findDuplicate(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-        slow = nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+    public static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    public static int findDuplicate(int[] nums) {
+        int i=0;
+        while(i< nums.length){
+            if(nums[i]-1!=i&&nums[i]!=nums[nums[i]-1]){
+                swap(nums, i, nums[i]-1);
+            }
+            else i++;
         }
-        return slow;
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j]-1!=j) return nums[j];
+        }
+        return -1;
     }
 }
 
