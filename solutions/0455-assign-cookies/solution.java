b376feb4eee7babb1e3282a8080bad5f37c2532c
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-10 10:42:53 +0000

    [LeetCode] Runtime - 10 ms (99.13%), Memory - 48.3 MB (100.00%)

---
+++ solutions/0455-assign-cookies/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int findContentChildren(int[] g, int[] s) {
+        int cookiesNums = s.length;
+        if(cookiesNums == 0)  return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int maxNum = 0;
+        int cookieIndex = cookiesNums - 1;
+        int childIndex = g.length - 1;
+        while(cookieIndex >= 0 && childIndex >=0){
+            if(s[cookieIndex] >= g[childIndex]){
+                maxNum++;
+                cookieIndex--;
+            }
+            childIndex--;
+        }
+        return maxNum;
+    }
+}
