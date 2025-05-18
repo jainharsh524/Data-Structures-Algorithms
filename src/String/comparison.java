fc0005f42ebc7eec3242f67f3ef13a1ecf6f9dd7
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-18 22:54:06 +0530

    Lecture - Sorting updated

---
+++ src/String/comparison.java
@@ -0,0 +1,20 @@
+package String;
+
+public class comparison {
+    public static void main(String[] args) {
+        String a = "Harsh";
+        String b = "Harsh";
+        String c = a;
+//        System.out.println(c == a);
+        // ==
+//        System.out.println(a == b);
+
+        String name1 = new String("Harsh");
+        String name2 = new String("Harsh");
+
+        System.out.println(name1 == name2);
+
+        System.out.println(name1.equals(name2));
+
+    }
+}
