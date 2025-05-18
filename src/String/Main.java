d4ca18a671df90ad1df29a190715d7ee1c408ecd
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-18 22:53:46 +0530

    Lecture - Sorting updated

---
+++ src/String/Main.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Harsh Jain";
+        System.out.println(name);
+
+        String a = "Harsh";
+        System.out.println(a);
+        a = "Jain";
+        System.out.println(a);
+    }
+}
