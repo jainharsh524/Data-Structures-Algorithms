3a677901e068083983ffd0fedeed573fa44af686
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-07 20:53:17 +0530

    Lecture - Recursion updated

---
+++ src/Math/oddEven.java
@@ -0,0 +1,10 @@
+package Math;
+
+public class oddEven {
+    public static void main(String[] args) {
+        int n = 73;
+        System.out.println((n&1)==1);// A method to find whether the number is odd or even
+        //Using Bitwise Operator AND.
+
+    }
+}
