53a178a5f375a558762d9ecff5bf873ae3dd0106
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-10 22:40:02 +0530

    Lecture - Math updated

--- src/Math/findUniqueUsingBitwiseInAnArray.java
+++ src/Math/findUniqueUsingBitwiseInAnArray.java
@@ -1,6 +1,7 @@
 package Math;
 
 public class findUniqueUsingBitwiseInAnArray {
+    //If every element is occurring twice
     public static int findUnique(int[] arr){
         int a = arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -8,7 +9,22 @@ public class findUniqueUsingBitwiseInAnArray {
         }
         return a;
     }
+    //If every element is occurring thrice
+    public static int findUniqueInTripleOccurrence(int[] nums){
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int num : nums) {
+                sum += (num >> i) & 1;
+            }
+            sum %= 3;
+            System.out.println(sum);
+            result |= (sum << i);
+//            System.out.println(result);
+        }
+        return result;
+    }
     public static void main(String[] args) {
-        System.out.println(findUnique(new int[]{1,1,2,3,3,4,4}));
+        System.out.println(findUniqueInTripleOccurrence(new int[]{1,1,1,3,3,3,4}));
     }
 }
