db5ea4c6a09cfcd61daa96080ac57d8c9e89224f
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-11 18:16:30 +0530

    Lecture - Math updated

---
+++ src/Math/findNumbersOfDigitsInAnyBase.java
@@ -0,0 +1,10 @@
+package Math;
+
+public class findNumbersOfDigitsInAnyBase {
+    public static int noOfDigits(int n, int base){
+        return (int) (Math.log(n)/Math.log(base)); //formula for finding the number of digits in any base
+    }
+    public static void main(String[] args) {
+
+    }
+}
