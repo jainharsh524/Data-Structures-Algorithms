02436b7942c33df897785e22fc5cc4ebfc0e32c0
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-11 18:14:19 +0530

    Lecture - Math updated

--- src/Math/findNThMagicNumber.java
+++ src/Math/findNThMagicNumber.java
@@ -1,7 +1,19 @@
 package Math;
 
+//Asked in amazon
 public class findNThMagicNumber {
+    public static int magicNumber(int n){
+        int sum = 0;
+        int count = 1;
+        while(n>=1){
+            int dig = n&1;
+            sum = (int) (sum+dig*Math.pow(5,count));
+            n = n>>1;
+            count++;
+        }
+        return sum;
+    }
     public static void main(String[] args) {
-
+        System.out.println(magicNumber(1));
     }
 }
