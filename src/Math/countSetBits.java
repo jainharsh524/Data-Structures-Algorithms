28c20ccbcef9e68e42894a26fbd78857ebb18354
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-14 16:58:40 +0530

    Lecture - Math updated

--- src/Math/countSetBits.java
+++ src/Math/countSetBits.java
@@ -1,7 +1,20 @@
 package Math;
 
 public class countSetBits {
-    public static void main(String[] args) {
+    public static int countBits(int num){
+        int count = 0;
+        while (num>0){
+            int end = num&1;
+            if(end==1) count++;
+            num = num>>1;
+            // Another way of doing it
+//            count++;
+//            num = num&(num-1);
+        }
 
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countBits(63));
     }
 }
