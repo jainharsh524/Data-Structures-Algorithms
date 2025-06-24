240c82677d4ccee87d0037475863237e0fa774e8
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-24 15:02:57 +0530

    Lecture - Math updated

---
+++ src/Math/GCD_LCM.java
@@ -0,0 +1,14 @@
+package Math;
+
+public class GCD_LCM {
+    static int gcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b%a, a);
+    }
+
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
