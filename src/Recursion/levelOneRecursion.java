5392acfd35635b34f335094cc3fe9f4edb0510d5
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-25 13:13:58 +0530

    Lecture - Recursion updated

--- src/Recursion/levelOneRecursion.java
+++ src/Recursion/levelOneRecursion.java
@@ -34,6 +34,22 @@ public class levelOneRecursion {
         }
         return sum;
     }
+    //6. CountZeroes
+    public static int countZeroes(int n) {
+        return helper(n, 0);
+    }
+
+    // special pattern, how to pass a value to above calls
+    private static int helper(int n, int c) {
+        if (n == 0) {
+            return c;
+        }
+        int rem = n % 10;
+        if (rem == 0) {
+            return helper(n/10, c+1);
+        }
+        return helper(n/10, c);
+    }
     public static void main(String[] args) {
         printTopToBottom(5);
         System.out.println();
