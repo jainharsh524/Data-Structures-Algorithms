34a36b48d21b13b21ddb09eb052dc7f5a9c89698
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-25 13:07:35 +0530

    Lecture - Math updated

---
+++ src/Recursion/levelOneRecursion.java
@@ -0,0 +1,46 @@
+package Recursion;
+
+public class levelOneRecursion {
+    //1. n to 1
+    public static void printTopToBottom(int n){
+        System.out.print(n+" ");
+        if(n==1) return;
+        printTopToBottom(n-1);
+    }
+    //2. 1 to n
+    public static void printBottomToTop(int n){
+        if(n==0) return;
+        printBottomToTop(n-1);
+        System.out.print(n+" ");
+    }
+    //3. ProductTillN Till N
+    public static int productTillN(int n){
+        if(n==1) return n;
+        else return n*productTillN(n-1);
+    }
+    //4. Sum of digits
+    public static int sumOfDigits(int n){
+        if(n==0) return n;
+        else return n%10+sumOfDigits(n/10);
+    }
+    //5. Reverse a number
+    static int sum;
+    public static int reverseInteger(int n){
+        if(n == 0) return n;
+        else {
+            int rem = n%10;
+            sum = sum*10+ rem;
+            reverseInteger(n/10);
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        printTopToBottom(5);
+        System.out.println();
+        printBottomToTop(5);
+        System.out.println();
+        System.out.println(productTillN(5));
+        System.out.println(sumOfDigits(594));
+        System.out.println(reverseInteger(594));
+    }
+}
