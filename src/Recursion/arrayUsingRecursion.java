0565c227ed8a61bc576a96446c8dcb02cb35aa21
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-27 22:23:15 +0530

    Lecture - Recursion updated

---
+++ src/Recursion/arrayUsingRecursion.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class arrayUsingRecursion {
+    //1. Check whether array is sorted or not.
+    public static boolean sorted(int[] arr){
+        return helper(arr, 1);
+    }
+    private static boolean helper(int[] arr, int currIndex){
+        if(currIndex== arr.length) return true;
+        else {
+            if (arr[currIndex] < arr[currIndex - 1]) return false;
+            else return helper(arr, currIndex + 1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(sorted(new int[]{1,2,3,2,5}));
+    }
+}
