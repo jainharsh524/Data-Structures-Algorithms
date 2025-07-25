af942788c4e289847a50bbb5a2105e59ee5ea0aa
jainharsh524 <harshnareshjain1729@gmail.com>
2025-07-25 03:57:07 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 7.8 MB (100.00%)

---
+++ solutions/0957-minimum-add-to-make-parentheses-valid/solution.c
@@ -0,0 +1,17 @@
+
+int minAddToMakeValid(char *s) {
+    int open = 0, close = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '(') {
+            open++;
+        } else if (s[i] == ')') {
+            if (open > 0) {
+                open--;
+            } else {
+                close++;
+            }
+        }
+    }
+    return open + close;
+}
