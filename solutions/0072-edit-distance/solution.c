5b0dbe3e42370a9a7b00994bd337de82ea30032c
jainharsh524 <harshnareshjain1729@gmail.com>
2025-08-03 17:18:39 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 9.5 MB (95.85%)

---
+++ solutions/0072-edit-distance/solution.c
@@ -0,0 +1,34 @@
+int dp[501][501];  // Adjust size if needed
+
+int min(int a, int b, int c) {
+    if (a <= b && a <= c) return a;
+    if (b <= a && b <= c) return b;
+    return c;
+}
+
+int solve(int i, int j, char* word1, char* word2) {
+    if (i < 0) return j + 1;  // Insert all remaining characters of word2
+    if (j < 0) return i + 1;  // Delete all remaining characters of word1
+
+    if (dp[i][j] != -1) return dp[i][j];
+
+    if (word1[i] == word2[j])
+        return dp[i][j] = solve(i - 1, j - 1, word1, word2);
+
+    int insertOp = solve(i, j - 1, word1, word2);
+    int deleteOp = solve(i - 1, j, word1, word2);
+    int replaceOp = solve(i - 1, j - 1, word1, word2);
+
+    return dp[i][j] = 1 + min(insertOp, deleteOp, replaceOp);
+}
+
+int minDistance(char* word1, char* word2) {
+    int n = strlen(word1);
+    int m = strlen(word2);
+
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= m; j++)
+            dp[i][j] = -1;
+
+    return solve(n - 1, m - 1, word1, word2);
+}
