78f7cc1e6ea35a5cbb4bba46fcc6bb200e9df04b
jainharsh524 <harshnareshjain1729@gmail.com>
2025-09-03 08:52:43 +0000

    [LeetCode] Runtime - 11 ms (20.63%), Memory - 19.1 MB (90.39%)

---
+++ solutions/0207-course-schedule/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        // Build graph & indegree count
+        for (auto &pre : prerequisites) {
+            int course = pre[0];
+            int prereq = pre[1];
+            graph[prereq].push_back(course);
+            indegree[course]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        int processed = 0;
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            processed++;
+
+            for (int neighbor : graph[curr]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) q.push(neighbor);
+            }
+        }
+
+        return processed == numCourses;
+    }
+};
+
