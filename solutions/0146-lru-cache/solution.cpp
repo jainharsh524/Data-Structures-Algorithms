c35f67554b962d0022d323ce44aad27a766379a3
jainharsh524 <harshnareshjain1729@gmail.com>
2026-03-16 06:25:39 +0000

    [LeetCode] Runtime - 67 ms (74.08%), Memory - 173 MB (86.91%)

--- solutions/0146-lru-cache/solution.cpp
+++ solutions/0146-lru-cache/solution.cpp
@@ -1,40 +1,72 @@
+struct Node{
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
+    Node(int k, int value){
+        key = k;
+        val = value;
+        prev = NULL;
+        next = NULL;
+    }
+};
 class LRUCache {
-    int cap;
-    list<int> lru; // keeps order: front = most recent
-    unordered_map<int, pair<int, list<int>::iterator>> cache;
-
 public:
+    unordered_map<int, Node*> mp;
+    int cap;
+    Node* head;
+    Node* tail;
     LRUCache(int capacity) {
         cap = capacity;
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head->next = tail;
+        tail->prev = head;
     }
 
+    void deleteNode(Node* nd){
+        Node* pre = nd->prev;
+        Node* nex = nd->next;
+        pre->next = nex;
+        nex->prev = pre;
+    }
+
+    void insertAfterHead(Node* nd){
+        Node* nex = head->next;
+        head->next = nd;
+        nd->prev = head;
+        nd->next = nex;
+        nex->prev = nd;
+    }
+    
     int get(int key) {
-        if (cache.find(key) == cache.end())
-            return -1;
-
-        // move key to front (most recently used)
-        lru.erase(cache[key].second);
-        lru.push_front(key);
-        cache[key].second = lru.begin();
-
-        return cache[key].first;
+        if(mp.count(key)==0) return -1;
+        else{
+            Node* nd = mp[key];
+            deleteNode(nd);
+            insertAfterHead(nd);
+            return nd->val;
+        }
     }
 
     void put(int key, int value) {
-        // if key already exists
-        if (cache.find(key) != cache.end()) {
-            lru.erase(cache[key].second);
+        if(mp.count(key)>0){
+            Node *nd = mp[key];
+            nd->val = value;
+            deleteNode(nd);
+            insertAfterHead(nd);
         }
-        // if cache is full
-        else if (cache.size() == cap) {
-            int lruKey = lru.back();
-            lru.pop_back();
-            cache.erase(lruKey);
+        else{
+            if(mp.size()==cap){
+                Node* nd = tail->prev;
+                mp.erase(nd->key);
+                deleteNode(nd);
+                // delete nd;
+            }
+            Node* nd = new Node(key, value);
+            mp[key]= nd;
+            insertAfterHead(nd);
         }
-
-        // insert/update key
-        lru.push_front(key);
-        cache[key] = {value, lru.begin()};
     }
 };
 
