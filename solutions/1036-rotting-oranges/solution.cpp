2cbf570ca6bf81caf1ee4f80bfcd6dc2f1058591
jainharsh524 <harshnareshjain1729@gmail.com>
2026-03-18 07:07:09 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 17.2 MB (29.37%)

--- solutions/1036-rotting-oranges/solution.cpp
+++ solutions/1036-rotting-oranges/solution.cpp
@@ -1,49 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
+    // void dfs(vector<vector<int>>& grid, int r, int c, vector<vector<int>>& count, int &cnt){
+    //     if(count[r][c]==0&&grid[r][c]==2){ 
+    //         count[r][c] = cnt;
+    //         vector<int> drow = {0,1,0,-1};
+    //         vector<int> dcol = {1,0,-1,0};
+    //         for(int i = 0;i<4;i++){
+    //             int nr = r+drow[i];
+    //             int nc = c+dcol[i];
+    //             if(nr>=0&&nc>=0&&nr<grid.size()&&nc<grid[0].size()){
+    //                 grid[nr][nc] = 2;
+    //                 dfs(grid, nr, nc, count, cnt+1);
+    //             }
+    //         }
+    //     }
+    //     else return;
+    // }
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int,int>> q;
-        int fresh = 0;
-        
-        // Initialize queue with rotten oranges and count fresh ones
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 2) {
-                    q.push({i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
+        queue<pair<pair<int, int>, int>> q;
+        for(int i = 0;i<grid.size();i++){
+            for(int j = 0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    q.push({{i, j}, 0});
                 }
             }
         }
-        
-        if (fresh == 0) return 0;  // No fresh oranges to rot
-        
-        int minutes = 0;
-        vector<int> dirs = {-1, 0, 1, 0, -1};
-        
-        // BFS: spread rot minute by minute
-        while (!q.empty() && fresh > 0) {
-            minutes++;
-            int qs = q.size();
-            while (qs--) {
-                auto [x, y] = q.front(); q.pop();
-                for (int d = 0; d < 4; ++d) {
-                    int nx = x + dirs[d];
-                    int ny = y + dirs[d+1];
-                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1) {
-                        grid[nx][ny] = 2;
-                        fresh--;
-                        q.push({nx, ny});
-                    }
+        int maxcnt = 0;
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c= q.front().first.second;
+            int cnt = q.front().second;
+            maxcnt = max(cnt, maxcnt);
+            q.pop();
+            vector<int> drow = {0,1,0,-1};
+            vector<int> dcol = {1,0,-1,0};
+            for(int i = 0;i<4;i++){
+                int nr = r+drow[i];
+                int nc = c+dcol[i];
+                if(nr>=0&&nc>=0&&nr<grid.size()&&nc<grid[0].size()&&grid[nr][nc]==1){
+                    grid[nr][nc] = 2;
+                    q.push({{nr, nc}, cnt+1});
                 }
             }
         }
-        
-        return fresh == 0 ? minutes : -1;
+        for(int i = 0;i<grid.size();i++){
+            for(int j = 0; j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        return maxcnt;
     }
 };
-
