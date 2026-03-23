eaa56e1e4a8169ae9f1db60319c50cb52cdb973c
jainharsh524 <harshnareshjain1729@gmail.com>
2026-03-23 06:05:01 +0000

    [LeetCode] Runtime - 7 ms (63.72%), Memory - 11.6 MB (76.85%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.cpp
+++ solutions/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map <char, int> mp;
+        unordered_map<char, int> mp;
         int len = 0;
         int maxlen = 0;
         int LU = 0;
         for(int i = 0;i<s.size();i++){
-            if(mp.find(s[i])!=mp.end()){
-                LU = max(LU, mp[s[i]]+1);
+            if(mp.find(s[i])==mp.end()){
+                mp[s[i]] = i;
             }
-            mp[s[i]] = i;
-            maxlen = max(i-LU+1, maxlen);
+            else{
+                LU = max(LU, mp[s[i]]+1);
+                mp[s[i]] = i;
+            }
+            len = i-LU+1;
+            maxlen = max(len, maxlen);
         }
         return maxlen;
     }
