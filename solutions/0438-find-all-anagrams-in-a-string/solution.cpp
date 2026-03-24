858138cd568912849d09692ebd335cdd0be23458
jainharsh524 <harshnareshjain1729@gmail.com>
2026-03-24 10:11:15 +0000

    [LeetCode] Runtime - 3 ms (80.27%), Memory - 11.5 MB (85.18%)

--- solutions/0438-find-all-anagrams-in-a-string/solution.cpp
+++ solutions/0438-find-all-anagrams-in-a-string/solution.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map <char, int> mp;
-        for( char ch: p){
-            mp[ch]++;
+        int m = p.size();
+        vector<int> res;
+        unordered_map<char, int> mp;
+        for(int i = 0;i<m;i++){
+            mp[p[i]]++;
         }
-        int left = 0;
-        int count = p.size();
-        vector<int>res;
-        for(int right = 0;right<s.size();right++){
-            if(mp[s[right]]>0) count--;
-            mp[s[right]]--;
-            if(right-left+1>p.size()){
-                mp[s[left]]++;
-                if(mp[s[left]]>0) count++;
-                left++;
+        int l = 0;
+        int required = m;
+        for(int r = 0;r<s.size();r++){
+            if(mp[s[r]]>0) required--;
+            mp[s[r]]--;
+            if(r-l+1>m){
+                mp[s[l]]++;
+                l++;
+                if(mp[s[l-1]]>0){
+                    required++;
+                }
             }
-            if(count==0){
-                res.push_back(left);
+            if(required==0){
+                res.push_back(l);
             }
         }
         return res;
