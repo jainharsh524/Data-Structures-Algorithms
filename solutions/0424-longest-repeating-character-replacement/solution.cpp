3cf10b6b5ef4a482d5b003e21dac3bc3460d2dcc
jainharsh524 <harshnareshjain1729@gmail.com>
2026-03-25 04:53:06 +0000

    [LeetCode] Runtime - 10 ms (33.07%), Memory - 11 MB (19.26%)

--- solutions/0424-longest-repeating-character-replacement/solution.cpp
+++ solutions/0424-longest-repeating-character-replacement/solution.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-     int characterReplacement(string s, int k) {
-        unordered_map<char, int> alphabets;
-        int ans = 0;
-        int left = 0;
-        int right = 0;
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> mp;
+        int l = 0;
+        int maxlen = 0;
         int maxf = 0;
-
-        for (right = 0; right < s.size(); right++) {
-            alphabets[s[right]] = 1 + alphabets[s[right]];
-            maxf = max(maxf, alphabets[s[right]]);
-
-            if ((right - left + 1) - maxf > k) {
-                alphabets[s[left]] -= 1;
-                left++;
-            } else {
-                ans = max(ans, (right - left + 1));
+        for(int r = 0;r<s.size();r++){
+            mp[s[r]]++;
+            maxf = max(maxf, mp[s[r]]);
+            if((r-l+1)>(k+maxf)){
+                mp[s[l]]--;
+                l++;
+            }
+            else{
+                maxlen = max(maxlen, r-l+1);
             }
         }
-
-        return ans;
+        return maxlen;
     }
 };
-
