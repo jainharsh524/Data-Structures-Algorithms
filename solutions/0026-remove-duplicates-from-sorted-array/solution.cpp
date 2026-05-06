a587ae815661271f4aa27a0f546d648865e2dc96
jainharsh524 <harshnareshjain1729@gmail.com>
2026-05-06 05:58:22 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 22.8 MB (9.29%)

--- solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    void swap(vector<int>&nums, int index1, int index2){
+    void swap(vector<int>& nums, int index1, int index2){
         int temp = nums[index1];
         nums[index1] = nums[index2];
         nums[index2] = temp;
     }
     int removeDuplicates(vector<int>& nums) {
-        int lastUnique = 1;
-        // int curr = 0;
+        int lu = 0;
         for(int i = 1;i<nums.size();i++){
-            if(nums[i]!=nums[lastUnique-1]){
-                swap(nums, lastUnique, i);
-                // cout<<lastUnique<<endl;
-                lastUnique++;
+            if(nums[i]>nums[lu]){
+                swap(nums, i, lu+1);
+                lu = lu+1;
             }
         }
-        return lastUnique;
+        return lu+1;
     }
 };
