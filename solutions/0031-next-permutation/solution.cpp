cb415ef7fe88a997739fc86d14cb96eddf0ff6bd
jainharsh524 <harshnareshjain1729@gmail.com>
2026-05-08 13:24:38 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 15.6 MB (70.83%)

--- solutions/0031-next-permutation/solution.cpp
+++ solutions/0031-next-permutation/solution.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    void rev(vector<int> &nums, int index1, int index2){
-        int i = index1;int j = index2;
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-        i++;j--;
-    }
     void swap(vector<int> &nums, int index1, int index2){
-        int i = index1;int j = index2;
-        while(i<j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;j--;
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+    void rev(vector<int> &nums, int start, int end){
+        while(start < end){
+            swap(nums, start, end);
+            start++;
+            end--;
         }
     }
     void nextPermutation(vector<int>& nums) {
-        int j = nums.size()-1;
-        while(j>0&&nums[j-1]>=nums[j]) j--;
-        if(j==0) swap(nums, j, nums.size()-1);
+        int i = nums.size() - 1;
+        while(i > 0 && nums[i] <= nums[i - 1]){
+            i--;
+        }
+        if(i == 0){
+            rev(nums, 0, nums.size() - 1);
+        }
         else{
-            int i = nums.size()-1;
-            while(i>j-1&&nums[j-1]>=nums[i])i--;
-            rev(nums, j-1, i);
-            swap(nums, j, nums.size()-1);
+            int j = nums.size() - 1;
+            while(nums[j] <= nums[i - 1]){
+                j--;
+            }
+            swap(nums, i - 1, j);
+            rev(nums, i, nums.size() - 1);
         }
     }
 };
