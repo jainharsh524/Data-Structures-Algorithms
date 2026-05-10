d95311f5ddbb46df7da7760ffa22ddafeaaa99f9
jainharsh524 <harshnareshjain1729@gmail.com>
2026-05-10 11:09:03 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 9.7 MB (63.59%)

---
+++ solutions/0118-pascals-triangle/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        if(numRows==0) return {{}};
+        res.push_back({1});
+        if(numRows==1) return res;
+        res.push_back({1, 1});
+        for(int i = 2;i<numRows;i++){
+            vector<int> temp(i+1);
+            temp[0] = 1;
+            temp[i] = 1;
+            for(int j = 1;j<i;j++){
+                temp[j] = res[i-1][j-1]+res[i-1][j];
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
