06f927bb8b830c2fb54c076e668915a6ff2c6c1a
jainharsh524 <harshnareshjain1729@gmail.com>
2026-05-26 18:09:02 +0000

    [LeetCode] Runtime - 4 ms (23.48%), Memory - 9.4 MB (28.94%)

--- solutions/0205-isomorphic-strings/solution.cpp
+++ solutions/0205-isomorphic-strings/solution.cpp
@@ -3,13 +3,17 @@ public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char, char> mps;
         for(int i = 0;i<s.size();i++){
-            if(mps.find(s[i])==mps.end()) mps.insert({s[i], t[i]});
-            else if(mps[s[i]]!=t[i]) return false;
+            if(mps.find(s[i])!=mps.end()){
+                if(mps[s[i]]!=t[i]) return false;
+            }
+            else mps[s[i]] = t[i]; 
         }
         unordered_map<char, char> mpt;
         for(int i = 0;i<s.size();i++){
-            if(mpt.find(t[i])==mpt.end()) mpt.insert({t[i], s[i]});
-            else if(mpt[t[i]]!=s[i]) return false;
+            if(mpt.find(t[i])!=mps.end()){
+                if(mpt[t[i]]!=s[i]) return false;
+            }
+            else mpt[t[i]] = s[i]; 
         }
         return true;
     }
