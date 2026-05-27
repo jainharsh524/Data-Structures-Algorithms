8e6f5feb89c226e2731eb7ee4c5ed8ad8cad1db2
jainharsh524 <harshnareshjain1729@gmail.com>
2026-05-27 10:06:44 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 10 MB (94.26%)

---
+++ solutions/0013-roman-to-integer/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+        for(int i = s.size()-1;i>=0;i--){
+            if(s[i]=='M') res+=1000;
+            else if(s[i]=='D') res+=500;
+            else if(s[i]=='C'){
+                if(i<s.size()-1&&(s[i+1]=='D'||s[i+1]=='M'))res-=100;
+                else res+=100;
+            }
+            else if(s[i]=='L') res+=50;
+            else if(s[i]=='X'){
+                if(i<s.size()-1&&(s[i+1]=='L'||s[i+1]=='C'))res-=10;
+                else res+=10;
+            }
+            else if(s[i]=='V') res+=5;
+            else if(s[i]=='I'){
+                if(i<s.size()-1&&(s[i+1]=='V'||s[i+1]=='X'))res-=1;
+                else res+=1;
+            }
+        }
+        return res;
+    }
+};
