49cb55d8f095550ae5938e36defb99168eaa38f0
jainharsh524 <harshnareshjain1729@gmail.com>
2026-05-29 16:48:51 +0000

    [LeetCode] Runtime - 4 ms (49.91%), Memory - 118 MB (65.60%)

--- solutions/0234-palindrome-linked-list/solution.cpp
+++ solutions/0234-palindrome-linked-list/solution.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        // Step 1: find length
-        int size = 0;
-        ListNode* curr = head;
-        while (curr) {
-            size++;
-            curr = curr->next;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
         }
 
-        // Step 2: move to middle
-        curr = head;
-        for (int i = 0; i < (size - 1) / 2; i++) {
-            curr = curr->next;
+        ListNode* prev = nullptr;
+        while (slow) {
+            ListNode* temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
         }
 
-        // Step 3: reverse second half
-        ListNode* prev = NULL;
-        ListNode* next = NULL;
-        ListNode* second = curr->next;  // start of second half
-        curr->next = NULL;              // disconnect first half
+        ListNode* first = head;
+        ListNode* second = prev;
 
         while (second) {
-            next = second->next;
-            second->next = prev;
-            prev = second;
-            second = next;
-        }
-
-        // Step 4: compare halves
-        ListNode* first = head;
-        ListNode* secondHalf = prev;
-
-        while (secondHalf) {
-            if (first->val != secondHalf->val)
+            if (first->val != second->val) {
                 return false;
+            }
             first = first->next;
-            secondHalf = secondHalf->next;
+            second = second->next;
         }
 
-        return true;
+        return true;        
     }
 };
-
