9d1e7d999826c7b52dbab48d19d2c05464ded178
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-07 10:22:52 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 19.1 MB (14.28%)

--- solutions/0023-merge-k-sorted-lists/solution.cpp
+++ solutions/0023-merge-k-sorted-lists/solution.cpp
@@ -1,35 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<
-            pair<int,int>,
-            vector<pair<int,int>>,
-            greater<pair<int,int>>
-        > pq;
-        for (int i = 0; i < lists.size(); i++) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(int i = 0;i<lists.size();i++){
             if (lists[i] != NULL) {
                 pq.push({lists[i]->val, i});
                 lists[i] = lists[i]->next;
             }
         }
-
         ListNode dummy(0);
         ListNode* curr = &dummy;
-
         while (!pq.empty()) {
             auto [val, idx] = pq.top();
             pq.pop();
-
             curr->next = new ListNode(val);
             curr = curr->next;
-
             if (lists[idx] != NULL) {
                 pq.push({lists[idx]->val, idx});
                 lists[idx] = lists[idx]->next;
             }
         }
-
         return dummy.next;
     }
 };
-
