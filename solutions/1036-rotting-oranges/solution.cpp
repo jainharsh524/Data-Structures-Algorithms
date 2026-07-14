4e02117f8e5c1fe1641e4201a4719f1adc9df1fa
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-14 15:21:14 +0000

    [LeetCode] Runtime - 3 ms (26.64%), Memory - 17.2 MB (29.26%)

--- solutions/1036-rotting-oranges/solution.cpp
+++ solutions/1036-rotting-oranges/solution.cpp
@@ -1,55 +1,35 @@
 class Solution {
 public:
-    // void dfs(vector<vector<int>>& grid, int r, int c, vector<vector<int>>& count, int &cnt){
-    //     if(count[r][c]==0&&grid[r][c]==2){ 
-    //         count[r][c] = cnt;
-    //         vector<int> drow = {0,1,0,-1};
-    //         vector<int> dcol = {1,0,-1,0};
-    //         for(int i = 0;i<4;i++){
-    //             int nr = r+drow[i];
-    //             int nc = c+dcol[i];
-    //             if(nr>=0&&nc>=0&&nr<grid.size()&&nc<grid[0].size()){
-    //                 grid[nr][nc] = 2;
-    //                 dfs(grid, nr, nc, count, cnt+1);
-    //             }
-    //         }
-    //     }
-    //     else return;
-    // }
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<pair<int, int>, int>> q;
-        for(int i = 0;i<grid.size();i++){
-            for(int j = 0;j<grid[0].size();j++){
-                if(grid[i][j]==2){
-                    q.push({{i, j}, 0});
-                }
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(grid[i][j] == 2) q.push({{i, j}, 0});
             }
         }
-        int maxcnt = 0;
+        int maxTimer = 0;
         while(!q.empty()){
             int r = q.front().first.first;
-            int c= q.front().first.second;
-            int cnt = q.front().second;
-            maxcnt = max(cnt, maxcnt);
+            int c = q.front().first.second;
+            int timer = q.front().second;
             q.pop();
-            vector<int> drow = {0,1,0,-1};
-            vector<int> dcol = {1,0,-1,0};
+            maxTimer = max(timer, maxTimer);
+            vector<int> drow = {0, -1, 0, 1};
+            vector<int> dcol = {1, 0, -1, 0};
             for(int i = 0;i<4;i++){
                 int nr = r+drow[i];
                 int nc = c+dcol[i];
                 if(nr>=0&&nc>=0&&nr<grid.size()&&nc<grid[0].size()&&grid[nr][nc]==1){
                     grid[nr][nc] = 2;
-                    q.push({{nr, nc}, cnt+1});
+                    q.push({{nr, nc}, timer+1});
                 }
             }
         }
         for(int i = 0;i<grid.size();i++){
-            for(int j = 0; j<grid[0].size();j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
+            for(int j = 0;j<grid[0].size();j++){
+                if(grid[i][j]==1) return -1;
             }
         }
-        return maxcnt;
+        return maxTimer;
     }
 };
