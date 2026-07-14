8f90fff69e67c0b2072ef46f3294a68f1d955919
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-14 06:02:30 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 21.6 MB (76.34%)

--- solutions/0057-insert-interval/solution.cpp
+++ solutions/0057-insert-interval/solution.cpp
@@ -1,33 +1,24 @@
 class Solution {
-private:
-    template <class It, class V, class L>
-    static It find(It beg, const It end, const V &k, const L cmp) __attribute__((always_inline)) {
-        if (beg == end) {
-            return beg;
-        } else if (beg + 8u > end) {
-            while (beg < end && cmp(*beg, k)) beg++;
-            return beg;
-        } else {
-            return lower_bound(beg, end, k, cmp);
-        }
-    }
-
 public:
-    static vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newint) {
-        const auto beg = intervals.begin(), end = intervals.end();
-        const auto its = find(beg, end, newint[0], [](const vector<int> &v, const int k) __attribute__((always_inline)) {
-            return v[1] < k;
-        });
-        const auto ite = find(its, end, newint[1] + 1, [](const vector<int> &v, const int k) __attribute__((always_inline)) {
-            return v[0] < k;
-        });
-        if (its != ite) {
-            (*its)[0] = min((*its)[0], newint[0]);
-            (*its)[1] = max((ite[-1])[1], newint[1]);
-            intervals.erase(its + 1u, ite);
-        } else {
-            intervals.insert(its, move(newint));
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int start = newInterval[0];
+        int end = newInterval[1];
+        int i = 0;
+        while(i<intervals.size()&&intervals[i][1]<start){
+            res.push_back(intervals[i]);
+            i++;
         }
-        return move(intervals);
+        while(i<intervals.size()&&intervals[i][0]<=end){
+            start = min(intervals[i][0], start);
+            end = max(intervals[i][1], end);
+            i++;
+        }
+        res.push_back({start, end});
+        while(i<intervals.size()){
+            res.push_back(intervals[i]);
+            i++;
+        }
+        return res;
     }
 };
