c5db3b95ff347341967f1e70b1a69b0d1ce47d08
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-16 09:37:01 +0000

    [LeetCode] Runtime - 68 ms (97.14%), Memory - 44.6 MB (45.03%)

--- solutions/0744-network-delay-time/solution.cpp
+++ solutions/0744-network-delay-time/solution.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        const int INF = 1e9;
-        vector<int> dist(n + 1, INF);
+        vector<int> dist(n + 1, 1e9);
+        vector<vector<pair<int,int>>> adj_lst(n + 1);
+        for(auto &f : times){
+            adj_lst[f[0]].push_back({f[1], f[2]});
+        }
+        priority_queue<
+            pair<int,int>,
+            vector<pair<int,int>>,
+            greater<pair<int,int>>
+        > pq;
+        pq.push({0, k});
         dist[k] = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (auto &e : times) {
-                int u = e[0], v = e[1], w = e[2];
-                if (dist[u] != INF && dist[u] + w < dist[v]) {
-                    dist[v] = dist[u] + w;
+        while(!pq.empty()){
+            auto [timer, node] = pq.top();
+            pq.pop();
+            if(timer > dist[node]) continue;
+            for(auto &nei : adj_lst[node]){
+                int neigh = nei.first;
+                int wt = nei.second;
+                if(dist[neigh] > timer + wt){
+                    dist[neigh] = timer + wt;
+                    pq.push({dist[neigh], neigh});
                 }
             }
         }
-
-        int maxTime = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == INF) return -1;
-            maxTime = max(maxTime, dist[i]);
+        int maxi = 0;
+        for(int i = 1; i <= n; i++){
+            if(dist[i] == 1e9) return -1;
+            maxi = max(maxi, dist[i]);
         }
-        return maxTime;
+        return maxi;
     }
 };
-
