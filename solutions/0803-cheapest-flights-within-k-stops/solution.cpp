85dd181a2fbb4cb245c5cd80c2807a7d2b4f6b66
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-16 08:24:38 +0000

    [LeetCode] Runtime - 6 ms (30.85%), Memory - 19.3 MB (16.56%)

--- solutions/0803-cheapest-flights-within-k-stops/solution.cpp
+++ solutions/0803-cheapest-flights-within-k-stops/solution.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        const int INF = INT_MAX / 2;
-        vector<int> dist(n, INF), temp(n);
-        dist[src] = 0;
-
-        for (int i = 0; i <= k; ++i) {
-            temp = dist;
-            for (auto &f : flights) {
-                int u = f[0], v = f[1], w = f[2];
-                if (dist[u] + w < temp[v]) {
-                    temp[v] = dist[u] + w;
+        vector<vector<pair<int,int>>> adj_lst(n);
+        for(auto &f : flights){
+            adj_lst[f[0]].push_back({f[1], f[2]});
+        }
+        priority_queue<
+            vector<int>,
+            vector<vector<int>>,
+            greater<vector<int>>
+        > pq;
+        // {cost, node, stops}
+        pq.push({0, src, 0});
+        vector<vector<int>> dist(n, vector<int>(k + 2, 1e9));
+        dist[src][0] = 0;
+        while(!pq.empty()){
+            auto cur = pq.top();
+            pq.pop();
+            int cost = cur[0];
+            int node = cur[1];
+            int stops = cur[2];
+            if(node == dst) return cost;
+            if(stops == k + 1) continue;
+            for(auto &nei : adj_lst[node]){
+                int next = nei.first;
+                int wt = nei.second;
+                if(cost + wt < dist[next][stops + 1]){
+                    dist[next][stops + 1] = cost + wt;
+                    pq.push({cost + wt, next, stops + 1});
                 }
             }
-            dist = temp;
         }
-
-        return (dist[dst] >= INF) ? -1 : dist[dst];
+        return -1;
     }
 };
-
