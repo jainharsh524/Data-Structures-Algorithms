b9bfd8ff35f866451fc75290b0a30c9af0647e5c
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-17 16:03:50 +0000

    [LeetCode] Runtime - 23 ms (51.30%), Memory - 24.7 MB (24.42%)

--- solutions/0984-most-stones-removed-with-same-row-or-column/solution.cpp
+++ solutions/0984-most-stones-removed-with-same-row-or-column/solution.cpp
@@ -1,21 +1,53 @@
+class DS{
+public:
+    vector<int> rank, parent;
+    DS(int n){
+        rank.resize(n,0);
+        parent.resize(n);
+        for(int i=0;i<n;i++) parent[i]=i;
+    }
+    int find_ult_par(int node){
+        if(node==parent[node]) return node;
+        return parent[node]=find_ult_par(parent[node]);
+    }
+    void unionByRank(int u,int v){
+        int pu=find_ult_par(u);
+        int pv=find_ult_par(v);
+        if(pu==pv) return;
+        if(rank[pu]>rank[pv]){
+            parent[pv]=pu;
+        }
+        else if(rank[pv]>rank[pu]){
+            parent[pu]=pv;
+        }
+        else{
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+    }
+};
 class Solution {
 public:
-    int dfs(vector<vector<int>>&stones,int index,vector<bool>&visited,int&n){
-        visited[index]=true;
-        int result=0;
-        for(int i=0;i<n;i++)
-            if(!visited[i]&&(stones[i][0]==stones[index][0]||stones[i][1]==stones[index][1]))
-                result +=(dfs(stones,i,visited,n) + 1);
-        return result;
-    }
-    int removeStones(vector<vector<int>>&stones) {
-        int n = stones.size();
-        vector<bool>visited(n,0);
-        int result=0;
-        for(int i=0;i<n;i++){
-            if(visited[i]){continue;}
-            result+=dfs(stones,i,visited,n);
+    int removeStones(vector<vector<int>>& stones) {
+        int maxR = 0;
+        int maxC = 0;
+        for(auto ele: stones){
+            maxR = max(maxR, ele[0]);
+            maxC = max(maxC, ele[1]);
         }
-        return result;
+        DS ds(maxR+maxC+2);
+        unordered_map<int, int> stoneNodes;
+        for(auto ele: stones){
+            int r = ele[0];
+            int c = ele[1]+maxR+1;
+            ds.unionByRank(r, c);
+            stoneNodes[r] = 1;
+            stoneNodes[c] = 1;
+        }
+        int cnt = 0;
+        for(auto ele: stoneNodes){
+            if(ds.find_ult_par(ele.first)==ele.first) cnt++;
+        }
+        return stones.size()-cnt;
     }
 };
