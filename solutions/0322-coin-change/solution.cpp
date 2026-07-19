1e3f486da011eebdc2ee5bbe31837afb55e6abeb
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-19 08:16:43 +0000

    [LeetCode] Runtime - 63 ms (10.38%), Memory - 49.7 MB (20.28%)

--- solutions/0322-coin-change/solution.cpp
+++ solutions/0322-coin-change/solution.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int index, int amount, vector<vector<int>>& dp) {
-        // ✅ base cases
-        if(amount == 0) return 0;              // FIXED
+    int solve(vector<int>& coins, int index, int amount, vector<vector<int>>& dp) { 
+        if(amount == 0) return 0;
         if(amount < 0) return INT_MAX;
-        if(index == coins.size()) return INT_MAX;
-
+        if(index == coins.size()) return INT_MAX; 
         if(dp[index][amount] != -1) 
             return dp[index][amount];
-
-        // take
+ 
         int take = solve(coins, index, amount - coins[index], dp);
-        if(take != INT_MAX) take = take + 1;
-
-        // skip
-        int skip = solve(coins, index + 1, amount, dp);
-
+        if(take != INT_MAX) take = take + 1; 
+        int skip = solve(coins, index + 1, amount, dp); 
         return dp[index][amount] = min(take, skip);
     }
 
