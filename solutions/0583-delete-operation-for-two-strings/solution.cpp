ef12f89ecf105e48e9a1313088bd0dbddb480a65
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-20 13:15:07 +0000

    [LeetCode] Runtime - 11 ms (63.57%), Memory - 16.6 MB (69.71%)

---
+++ solutions/0583-delete-operation-for-two-strings/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int commonsubseq(string &s1, string &s2, int index1, int index2, vector<vector<int>>& dp){
+        // if(index1>=s1.size()||index2>=s2.size()) return 0;
+        if(index1<0||index2<0) return 0;
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
+        if(s1[index1]==s2[index2]){
+            return dp[index1][index2] = 1+commonsubseq(s1, s2, index1-1, index2-1, dp);
+        }
+        else {
+            return dp[index1][index2] = max(commonsubseq(s1, s2, index1, index2-1, dp), commonsubseq(s1, s2, index1-1, index2, dp));
+        }
+    }
+    int minDistance(string s, string t) {
+        vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
+        int lcs = commonsubseq(s, t, s.size()-1, t.size()-1, dp);
+        return t.size()+s.size()-2*lcs;
+    }
+};
