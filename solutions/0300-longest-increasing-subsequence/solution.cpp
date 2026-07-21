c3463e58fa57efa52c31ccf35f7ba77f7a0cc23a
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-21 17:21:55 +0000

    [LeetCode] Runtime - 695 ms (5.06%), Memory - 296.6 MB (6.73%)

--- solutions/0300-longest-increasing-subsequence/solution.cpp
+++ solutions/0300-longest-increasing-subsequence/solution.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int count(vector<int>& nums, int index, int last, vector<vector<int>>& dp){
-        if(index>=nums.size()) return 0;
-        if(dp[index][last+1]!=-1) return dp[index][last+1];
-        else{
-            int take = 0;
-            int skip = 0;
-            if((last == -1)||(nums[index]>nums[last])){
-                take = 1 + count(nums, index+1, index, dp);
-            }
-            skip = count(nums, index+1, last, dp);
-            return dp[index][last+1] = max(take, skip);
+    int calen(vector<int>& nums, int index, int last, vector<vector<int>>& dp){
+        if(index==nums.size()) return 0;
+        if(dp[index][last + 1] != -1) return dp[index][last + 1];
+        if(last==-1||nums[index]>nums[last]){
+            return dp[index][last + 1] = max(1+calen(nums, index+1, index, dp), calen(nums, index+1, last, dp));
         }
-    }
+        else return dp[index][last + 1] = calen(nums, index+1, last, dp);
+    } 
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1, -1));
-        return count(nums, 0, -1, dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        return calen(nums, 0, -1, dp);
     }
 };
