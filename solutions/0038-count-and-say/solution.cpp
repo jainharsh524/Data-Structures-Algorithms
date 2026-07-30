d27adf7fc2817e16ce50d976fe87929f213791e3
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-30 10:55:09 +0000

    [LeetCode] Runtime - 66 ms (5.17%), Memory - 126.2 MB (5.27%)

--- solutions/0038-count-and-say/solution.cpp
+++ solutions/0038-count-and-say/solution.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    string countAndSay(int n) {
-        string res = "1";
-        string nres = "";
-        for(int i = 1;i<n;i++){
-            int count = 0;
-            char ele = res[0];
-            int sz = res.size();
-            for(int i = 0;i<sz;i++){
-                if(res[i]==ele){
-                    count++;
-                }
-                else{
-                    string ap = to_string(count)+res[i-1];
-                    nres.append(ap);
-                    count = 1;
-                    ele = res[i];
-                }
-            }
-            string ap = to_string(count)+ele;
-            nres.append(ap);
-            res = nres;
-            nres = "";
+    string res;
+    void CAS(int n, string& s){
+        if(n==1){
+            res = s;
+            return;
         }
+        string ns = "";
+        int l = 0;
+        int i = 0;
+        for(i = 0;i<s.size();i++){
+            if(s[l]!=s[i]){
+                ns = ns+to_string(i-l)+s[l];
+                l = i;
+            }
+        }
+        ns = ns+to_string(i-l)+s[l];
+        cout<<ns<<" "<<n<<endl;
+        return CAS(n-1, ns);
+    }
+    string countAndSay(int n) {
+        //"111221"->"312211"
+        string s = "1";
+        if(n==1) return s;
+        CAS(n, s);
         return res;
     }
 };
