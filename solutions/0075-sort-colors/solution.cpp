11c5bed53abee5d8625601f24f1124af8644668e
jainharsh524 <harshnareshjain1729@gmail.com>
2026-07-31 13:29:04 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 11.5 MB (79.68%)

--- solutions/0075-sort-colors/solution.cpp
+++ solutions/0075-sort-colors/solution.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    void swap(vector<int> &nums, int index1, int index2){
-        int temp = nums[index1];
-        nums[index1] = nums[index2];
-        nums[index2] = temp;
-    }
     void sortColors(vector<int>& nums) {
-        int low = 0;
-        int mid = 0;
-        int high = nums.size()-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums, low, mid);
-                low++;
-                mid++;
+        int l = 0;
+        int r = nums.size()-1;
+        int m = 0;
+        while(m<=r){
+            if(nums[m]==0){
+                swap(nums[l], nums[m]);l++;m++;
             }
-            else if(nums[mid]==2){
-                swap(nums, mid, high);
-                high--;
+            else if(nums[m]==2){
+                swap(nums[m], nums[r]);r--;
             }
-            else{
-                mid++;
-            }
+            else if(nums[m]==1)m++;
         }
     }
 };
