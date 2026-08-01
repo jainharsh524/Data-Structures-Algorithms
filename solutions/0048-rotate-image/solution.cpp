a4e36d2814a428c96958781316e2eff81a3bd0c1
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-01 09:03:20 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 10.1 MB (70.83%)

--- solutions/0048-rotate-image/solution.cpp
+++ solutions/0048-rotate-image/solution.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    void swap(vector<vector<int>>& matrix, int r1, int r2, int c1, int c2){
-        int temp = matrix[r1][c1];
-        matrix[r1][c1] = matrix[r2][c2];
-        matrix[r2][c2] = temp;
-    }
     void transpose(vector<vector<int>>& matrix){
         for(int i = 0;i<matrix.size();i++){
-            for(int j = i+1;j<matrix[0].size();j++){
-                swap(matrix, i, j, j, i);
-            }
-        }
-    }
-    void revHo(vector<vector<int>>& matrix){
-        for(int i = 0;i<matrix.size();i++){
-            int j1 = 0;
-            int j2 = matrix.size()-1;
-            while(j1<j2){
-                swap(matrix, i, i, j1, j2);
-                j1++;j2--;
+            for(int j = 0;j<i;j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
     }
     void rotate(vector<vector<int>>& matrix) {
         transpose(matrix);
-        revHo(matrix);
+        for(int i = 0;i<matrix.size();i++){
+            int l = 0;int r = matrix.size()-1;
+            while(l<r){
+                swap(matrix[i][l], matrix[i][r]);
+                l++;
+                r--;
+            }
+        }
     }
 };
