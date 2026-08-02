f7e711fbd3549ba1e9ff6657a24dbf415d25067e
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-02 06:26:32 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 8 MB (38.33%)

--- solutions/2050-count-good-numbers/solution.cpp
+++ solutions/2050-count-good-numbers/solution.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
-
-    int countGoodNumbers(long long chakraLength) {
-        long long even = (chakraLength + 1) / 2;
-        long long odd = chakraLength / 2;
-
-        long long evenWays = chakraPower(5, even);
-        long long oddWays = chakraPower(4, odd);
-
-        return (evenWays * oddWays) % MOD;
-    }
-
-    long long chakraPower(long long base, long long power) {
-        long long result = 1;
-        base %= MOD;
-
-        while (power > 0) {
-            if (power % 2 == 1)
-                result = (result * base) % MOD;
-
-            base = (base * base) % MOD;
-            power /= 2;
+    const int MOD = 1e9+7;
+    long long power(long long m, long long n){
+        long long res = 1;
+        while(n){
+            if(n % 2 == 0){
+                m = (m * m) % MOD;
+                n /= 2;
+            }
+            else{
+                res = (res * m) % MOD;
+                n--;
+            }
         }
-
-        return result;
+        return res;
+    }
+    int countGoodNumbers(long long n) {
+        long long evenpow = (n + 1) / 2;
+        long long primepow = n / 2;
+        long long val1 = power(5, evenpow);
+        long long val2 = power(4, primepow);
+        return (val1 * val2) % MOD;
     }
 };
