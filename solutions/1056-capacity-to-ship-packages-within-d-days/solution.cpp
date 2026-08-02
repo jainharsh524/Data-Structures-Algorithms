db1877b98561d6b34d5e0c34f08dafb3e510ca4e
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-02 05:32:41 +0000

    [LeetCode] Runtime - 2 ms (99.42%), Memory - 35 MB (89.01%)

--- solutions/1056-capacity-to-ship-packages-within-d-days/solution.cpp
+++ solutions/1056-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    int calDays(vector<int> &weights, int cap){
-        int currSum = 0;
-        int days = 0;
-        for(int ele: weights){
-            currSum = currSum+ele;
-            if(currSum>cap) {
-                currSum = ele;
-                days++;
-            }
-        }
-        if(currSum>0) days++;
-        return days;
-    }
-    int shipWithinDays(vector<int>& weights, int days) {
+    int daycal(vector<int>& weights, int capacity){
         int sum = 0;
-        int maxi = 0;
+        int cntdays = 0;
         for(int ele: weights){
             sum+=ele;
-            maxi = max(ele, maxi);
+            if(sum>capacity){
+                sum = ele;
+                cntdays++;
+            }
         }
-        if(days==1) return sum;
+        if(sum==0) return cntdays;
+        else return cntdays+1;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int maxi = 0;
+        int sum = 0;
+        for(int ele: weights) maxi = max(maxi, ele);
+        for(int ele: weights) sum+=ele;
         int l = maxi;
         int r = sum;
+        int ans = sum;
         while(l<r){
-            int m = l+(r-l)/2;
-            int d = calDays(weights, m);
-            if(d>days) l = m+1;
-            else r = m;
+            int midcap = l+(r-l)/2;
+            int day = daycal(weights, midcap);
+            // cout<<midcap<<" "<<l<<" "<<r<<" "<<day<<endl;
+            if(day>days) {
+                l = midcap+1;
+            }
+            else {
+                ans = min(ans, midcap);
+                r = midcap;
+            }
         }
-        return l;
+        return ans;
     }
 };
