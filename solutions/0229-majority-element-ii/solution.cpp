9cb71570f90e60602abd43bb7cc503f3f0b01cae
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-03 04:39:07 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 23.7 MB (69.47%)

--- solutions/0229-majority-element-ii/solution.cpp
+++ solutions/0229-majority-element-ii/solution.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int count1 = 0, count2 = 0; // Counters for the potential majority elements
-        int candidate1 = 0, candidate2 = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (count1 == 0 && nums[i] != candidate2) {
+        int count1 = 0;
+        int count2 = 0;
+        int cand1 = 0;
+        int cand2 = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(count1==0&&nums[i]!=cand2){ //curr number should not be equal to cand2;
+                cand1 = nums[i];
                 count1 = 1;
-                candidate1 = nums[i];
             }
-            else if (count2 == 0 && nums[i] != candidate1) {
+            else if(count2==0&&nums[i]!=cand1){ //curr number should not be equal to cand1;
+                cand2 = nums[i];
                 count2 = 1;
-                candidate2 = nums[i];
-            } 
-            else if (candidate1 == nums[i]) {
-                count1++;
-            } else if (candidate2 == nums[i]) {
-                count2++;
             }
-            else {
-                count1--;
-                count2--;
+            else if(nums[i]==cand1) count1++;
+            else if(nums[i]==cand2) count2++;
+            else{
+                count1--; count2--;
             }
         }
-
-        vector<int> result;
-        int threshold = nums.size() / 3;
-        count1 = 0, count2 = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (candidate1 == nums[i]) {
-                count1++;
-            } else if (candidate2 == nums[i]) {
-                count2++;
-            }
+        vector<int> res;
+        count1 = 0;
+        count2 = 0;
+        cout<<cand1<<" "<<cand2<<endl;
+        int thre = nums.size()/3;
+        for(int ele: nums){
+            if(ele==cand1) count1++;
+            else if(ele==cand2) count2++;
         }
-        if (count1 > threshold) {
-            result.push_back(candidate1);
-        }
-        if (count2 > threshold) {
-            result.push_back(candidate2);
-        }
-
-        return result;
+        if(count1>thre) res.push_back(cand1);
+        if(count2>thre) res.push_back(cand2);
+        return res;
     }
 };
