139a16fe5fb5017f6bd645007376881c848fb0c8
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-04 08:09:24 +0000

    [LeetCode] Runtime - 57 ms (54.82%), Memory - 24 MB (26.30%)

--- solutions/0160-intersection-of-two-linked-lists/solution.cpp
+++ solutions/0160-intersection-of-two-linked-lists/solution.cpp
@@ -9,19 +9,28 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA==NULL||headB==NULL) return NULL;
-        ListNode* ptra = headA;
-        ListNode* ptrb = headB;
-        while(ptra!=ptrb){
-            if(ptra==NULL){
-                ptra = headB;
-            }
-            else ptra = ptra->next;
-            if(ptrb==NULL){
-                ptrb = headA;
-            }
-            else ptrb = ptrb->next;
+        if(headA->next==NULL&&headB->next==NULL&&headA!=headB) return NULL;
+        ListNode* slow = headA;
+        while(slow->next) slow = slow->next;
+        ListNode* endA = slow;
+        slow->next = headB;
+        slow = headA;
+        ListNode* fast = headA;
+        while(slow->next&&fast->next&&fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast) break;
         }
-        return ptra;
+        if(slow!=fast){
+            endA->next = NULL;
+            return NULL;
+        }
+        slow = headA;
+        while(slow->next&&fast->next&&fast->next->next&&slow!=fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        endA->next = NULL;
+        return slow;
     }
 };
