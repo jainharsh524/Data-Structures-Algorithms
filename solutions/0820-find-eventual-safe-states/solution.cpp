26d5ac63a02ea800c6f808b6f9c83ec083905ab4
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-05 08:58:43 +0000

    [LeetCode] Runtime - 52 ms (35.65%), Memory - 66.3 MB (20.45%)

--- solutions/0820-find-eventual-safe-states/solution.cpp
+++ solutions/0820-find-eventual-safe-states/solution.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int nodes = graph.size();
-        vector<int> outdegree(nodes, 0);
-        vector<int> res;
-        vector<vector<int>> revg(nodes);
-        for(int i =0;i<nodes;i++){
-            outdegree[i] = graph[i].size();
+        vector<int> outdeg(graph.size());
+        for(int i = 0;i<graph.size();i++){
+            outdeg[i] = graph[i].size();
         }
         queue<int> q;
-        for(int i =0;i<nodes;i++){
-            if(outdegree[i]==0){
-                q.push(i);
-            }
-        }
         for(int i = 0;i<graph.size();i++){
-            for(int j = 0;j<graph[i].size();j++){
-                revg[graph[i][j]].push_back(i);
+            if(outdeg[i]==0) q.push(i);
+        }
+        vector<vector<int>> rev_adj(graph.size());
+        for(int i = 0;i<graph.size();i++){
+            for(int ele: graph[i]){
+                rev_adj[ele].push_back(i);
             }
         }
+        vector<int> res;
         while(!q.empty()){
-            int node = q.front();
+            int ele = q.front();
             q.pop();
-            res.push_back(node);
-            for(int nn: revg[node]){
-                outdegree[nn]--;
-                if(outdegree[nn]==0){
-                    q.push(nn);
-                }
+            res.push_back(ele);
+            for(int nei: rev_adj[ele]){
+                outdeg[nei]--;
+                if(outdeg[nei]==0) q.push(nei);
             }
         }
         sort(res.begin(), res.end());
