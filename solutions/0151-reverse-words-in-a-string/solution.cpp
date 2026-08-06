769cdd5d6e2b43a35482c7bbe75e398c2d300366
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-06 08:42:21 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 9.7 MB (98.49%)

--- solutions/0151-reverse-words-in-a-string/solution.cpp
+++ solutions/0151-reverse-words-in-a-string/solution.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        reverse(s.begin(), s.end());
-        int l = 0;
+        int i = 0, j = s.size() - 1;
         int r = 0;
-        while(l<s.size()){
-            if(s[l]==' '){
-                s.erase(r, 1);
-            }
-            else break;
-        }
-        while(r<s.size()){
-            if(s[r]==' '){
-                reverse(s.begin() + l, s.begin() + r);
+        while (r < s.size() && s[r] == ' ') r++;
+        s.erase(0, r);
+        int l = s.size() - 1;
+        while (l >= 0 && s[l] == ' ') l--;
+        s.erase(l + 1);
+        reverse(s.begin(), s.end());
+        i = 0;
+        r = 0;
+        while (r < s.size()) {
+            while (r < s.size() && s[r] != ' ')
                 r++;
-                while(r<s.size()&&s[r]==' '){
-                    s.erase(r, 1);
-                }
-                l = r;
-            }
-            else r++;
+            reverse(s.begin() + i, s.begin() + r);
+            int start = r;
+            while (r < s.size() && s[r] == ' ')
+                r++;
+            if (r - start > 1)
+                s.erase(start + 1, r - start - 1);
+            i = start + 1;
+            r = i;
         }
-        reverse(s.begin()+l, s.end());
-        r--;
-        if(s[r]==' ') s.erase(r, 1);
         return s;
     }
 };
