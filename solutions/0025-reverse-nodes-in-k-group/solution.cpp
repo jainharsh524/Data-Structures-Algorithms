a3c1203bb789660696b3468ef414851752dd0e39
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-09 08:35:47 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 16.4 MB (71.33%)

--- solutions/0025-reverse-nodes-in-k-group/solution.cpp
+++ solutions/0025-reverse-nodes-in-k-group/solution.cpp
@@ -11,33 +11,22 @@
 
 class Solution {
 public:
-
     // Reverse linked list from head till end(exclusive)
     ListNode* reverseK(ListNode* head, ListNode* end) {
-
         ListNode* prev = end;
         ListNode* curr = head;
-
         while (curr != end) {
-
             ListNode* nex = curr->next;
-
             curr->next = prev;
-
             prev = curr;
-
             curr = nex;
         }
-
         // prev becomes new head
         return prev;
     }
-
     ListNode* reverseKGroup(ListNode* head, int k) {
-
         if (!head || k == 1)
             return head;
-
         ListNode* curr = head;
         int count = 0;
         while (curr && count < k) {
