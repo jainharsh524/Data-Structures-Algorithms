48c81f8092226f882c431248b84c41427a076fd5
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-09 13:45:03 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 60.9 MB (53.34%)

--- solutions/0099-recover-binary-search-tree/solution.cpp
+++ solutions/0099-recover-binary-search-tree/solution.cpp
@@ -11,25 +11,32 @@
  */
 class Solution {
 public:
-    TreeNode *prev = NULL, *first = NULL, *second = NULL, *last = NULL;
-    void inord(TreeNode* root){
-        if(root==NULL) return;
-        //keep the inorder same, just track the violations, either 2 adjacent nodes value will be swapped or you will find the 2nd violation and in that case swap the first and last'
-        inord(root->left); 
-        if(prev!=NULL&&prev->val>root->val){
-            if(first==NULL){
-                first = prev;
-                second = root;
+    TreeNode* prev = nullptr;
+    TreeNode *first = nullptr, *sec = nullptr, *last = nullptr;
+    void checkIn(TreeNode* root){
+        if(root==nullptr) return;
+        checkIn(root->left);
+        if(prev){
+            if(prev->val>root->val){
+                if(!first){
+                    first = prev;
+                    sec = root;
+                }
+                else{
+                    last = root;
+                }
             }
-            else last = root;
         }
         prev = root;
-        inord(root->right);
+        checkIn(root->right);
     }
     void recoverTree(TreeNode* root) {
-        prev = new TreeNode(INT_MIN);
-        inord(root);
-        if(first&&last) swap(first->val, last->val);
-        else swap(first->val, second->val);
+        checkIn(root);
+        if(last){
+            swap(first->val, last->val);
+        }
+        else{
+            swap(first->val, sec->val);
+        }
     }
 };
