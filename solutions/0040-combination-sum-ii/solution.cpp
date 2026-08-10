d3eaa786901c191b05ddffc9fe12c79b3cbb1661
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-10 07:16:57 +0000

    [LeetCode] Runtime - 3 ms (37.74%), Memory - 15.4 MB (11.79%)

--- solutions/0040-combination-sum-ii/solution.cpp
+++ solutions/0040-combination-sum-ii/solution.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector < vector < int >> ans;
-        vector < int > ds;
-        findCombination(0, target, candidates, ans, ds);
-        return ans;
-    }
-    void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
-        if (target == 0) {
-            ans.push_back(ds);
+    void generate(vector<int>& candidates, int target, int index, vector<int> temp, vector<vector<int>>& res){
+        if(target==0){
+            res.push_back(temp);
             return;
         }
-        for (int i = ind; i < arr.size(); i++) {
-            if (i > ind && arr[i] == arr[i - 1]) continue;
-            if (arr[i] > target) break;
-            ds.push_back(arr[i]);
-            findCombination(i + 1, target - arr[i], arr, ans, ds);
-            ds.pop_back();
+        for(int i = index;i<candidates.size();i++){
+            if(i>index&&candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+            temp.push_back(candidates[i]);
+            generate(candidates,target-candidates[i],i+1,temp,res);
+            temp.pop_back();
         }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> res;
+        vector<int> temp;
+        generate(candidates, target, 0, temp, res);
+        return res;
     }
 };
