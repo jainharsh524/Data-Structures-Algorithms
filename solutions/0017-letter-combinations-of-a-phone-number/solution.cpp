b34c3cc19e1ac42a6ff6c6d33cd171523b44146a
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-10 07:39:29 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 9.2 MB (62.74%)

--- solutions/0017-letter-combinations-of-a-phone-number/solution.cpp
+++ solutions/0017-letter-combinations-of-a-phone-number/solution.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    void generate(string &digits, string temp, vector<string>& res, unordered_map<char, string> &mp, int index){
-        if(index==digits.size()){
-            res.push_back(temp);
-            return;
-        }
-        string val = mp[digits[index]];
+    void comb(string& digits, int index, unordered_map<int, string>& mp, string temp, vector<string>& res){
+        if(index==digits.size()) res.push_back(temp);
+        int num = digits[index] - '0';
+        string val = mp[num];
         for(char ch: val){
             temp.push_back(ch);
-            generate(digits, temp, res, mp, index+1);
+            comb(digits, index+1, mp, temp, res);
             temp.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        unordered_map<char, string> mp;
-        mp['2'] = "abc";
-        mp['3'] = "def";
-        mp['4'] = "ghi";
-        mp['5'] = "jkl";
-        mp['6'] = "mno";
-        mp['7'] = "pqrs";
-        mp['8'] = "tuv";
-        mp['9'] = "wxyz";
+        unordered_map<int, string> mp;
+        mp[2] = "abc";
+        mp[3] = "def";
+        mp[4] = "ghi";
+        mp[5] = "jkl";
+        mp[6] = "mno";
+        mp[7] = "pqrs";
+        mp[8] = "tuv";
+        mp[9] = "wxyz";
         vector<string> res;
-        string temp = "";
-        generate(digits, temp, res, mp, 0);
+        string temp;
+        comb(digits, 0, mp, temp, res);
         return res;
     }
 };
