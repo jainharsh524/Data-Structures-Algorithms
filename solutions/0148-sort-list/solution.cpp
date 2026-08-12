dd251d8f7f3e95adf65347058b1e752022ed3ae7
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-12 08:59:37 +0000

    [LeetCode] Runtime - 53 ms (20.38%), Memory - 75.6 MB (40.95%)

--- solutions/0148-sort-list/solution.cpp
+++ solutions/0148-sort-list/solution.cpp
@@ -1,37 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* merge(ListNode* list1,ListNode* list2){
-        ListNode dummy;
-        ListNode* prev=&dummy;
-        while(list1&&list2){
-            if(list1->val<list2->val){
-                prev->next=list1;
-                list1=list1->next;
-            }
-            else{
-                prev->next=list2;
-                list2=list2->next;
-            }
-            prev=prev->next;
+    ListNode* calmid(ListNode* head, ListNode* end) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (slow != end && fast != end && fast->next != end) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        prev->next=list1 ? list1 : list2;
-        return dummy.next;
+        return slow;
     }
-    ListNode* sorting(ListNode* head){
-        if(head==nullptr||head->next==nullptr) return head;
-        ListNode* slow=head;
-        ListNode* fast=head->next;
-        while(fast&&fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
+    ListNode* merge(ListNode* lcurr, ListNode* rcurr) {
+        ListNode* prev = new ListNode();
+        ListNode* dummy = prev;
+        while (lcurr && rcurr) {
+            if (lcurr->val < rcurr->val) {
+                prev->next = lcurr;
+                lcurr = lcurr->next;
+            } else {
+                prev->next = rcurr;
+                rcurr = rcurr->next;
+            }
+            prev = prev->next;
+            prev->next = nullptr;
         }
-        ListNode* right=slow->next;
-        slow->next=nullptr;
-        ListNode* left=sorting(head);
-        right=sorting(right);
-        return merge(left,right);
+        prev->next = (lcurr) ? lcurr : rcurr;
+        return dummy->next;
     }
-    ListNode* sortList(ListNode* head){
-        return sorting(head);
+    ListNode* sorting(ListNode* head, ListNode* end) {
+        if (head == end)
+            return nullptr; 
+        if (head->next == end) {
+            head->next = nullptr;
+            return head;
+        }
+        ListNode* mid = calmid(head, end);
+        // Sort [head, mid)
+        ListNode* left = sorting(head, mid);
+        // Sort [mid, end)
+        ListNode* right = sorting(mid, end);
+        return merge(left, right);
+    }
+    ListNode* sortList(ListNode* head) {
+        if (head == NULL)
+            return nullptr;
+        head = sorting(head, nullptr);
+        return head;
     }
 };
