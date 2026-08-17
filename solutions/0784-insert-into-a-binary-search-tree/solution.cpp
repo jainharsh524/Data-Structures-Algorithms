2e5afe6fc066fc84f92bd7db814af11cc5949b8b
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-17 09:17:02 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 59.3 MB (93.58%)

--- solutions/0784-insert-into-a-binary-search-tree/solution.cpp
+++ solutions/0784-insert-into-a-binary-search-tree/solution.cpp
@@ -12,14 +12,9 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root==NULL) return new TreeNode(val);
-        TreeNode* curr = root;
-        while(true){
-            if(curr->right&&curr->val<=val) curr = curr->right;
-            else if(curr->left&&curr->val>val) curr = curr->left;
-            else if(!curr->right&&curr->val<=val){ curr->right = new TreeNode(val); break;}
-            else if(!curr->left&&curr->val>val){ curr->left = new TreeNode(val); break;}
-        }
+        if(root == NULL) return new TreeNode(val);
+        if(root->val < val) root->right = insertIntoBST(root->right, val); 
+        else if(root->val > val) root->left = insertIntoBST(root->left, val);
         return root;
     }
 };
