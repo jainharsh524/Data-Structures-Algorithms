a10c3129c9b5624911e8d91fa87824ae87bfde20
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-18 13:52:24 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 28 MB (6.29%)

--- solutions/0042-trapping-rain-water/solution.cpp
+++ solutions/0042-trapping-rain-water/solution.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int total = 0;
-        int lm = 0;
-        int rm = 0;
-        int start = 0;
-        int end = height.size()-1;
-        while(start<end){
-            lm = max(lm, height[start]);
-            rm = max(rm, height[end]);
-            if(lm<rm){
-                total+=lm-height[start];
-                start++;
-            }
-            else{
-                total+=rm-height[end];
-                end--;
-            }
+        int l = 0, r = height.size()-1;
+        int sum = 0;
+        int pge = 0;
+        vector<int> pg(height.size(), 0);
+        for(int i = l;i<height.size();i++){
+            pge = max(height[i], pge);
+            pg[i] = pge;
         }
-        return total;
+        vector<int> ng(height.size(), 0);
+        int nge = 0;
+        for(int i = r;i>=0;i--){
+            nge = max(height[i], nge);
+            ng[i] = nge;
+        }
+        vector<int> res(height.size());
+        for(int i = 0;i<height.size();i++) res[i] = min(ng[i], pg[i]); 
+        for(int i = 0;i<height.size();i++) sum = sum+res[i]-height[i]; 
+        return sum;
     }
 };
