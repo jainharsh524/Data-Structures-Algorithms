e3f465a55154e1c45b0ee8fa491ce0318eae7dda
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-20 08:48:53 +0000

    [LeetCode] Runtime - 15 ms (25.48%), Memory - 22.9 MB (19.88%)

--- solutions/0085-maximal-rectangle/solution.cpp
+++ solutions/0085-maximal-rectangle/solution.cpp
@@ -1,66 +1,45 @@
 class Solution {
 public:
-    vector<int> nse(vector<int>& nums){
-        stack<pair<int, int>> st;
-        vector<int> res(nums.size());
-        for(int i = nums.size()-1;i>=0;i--){
-            while(!st.empty()&&st.top().first>=nums[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                st.push({nums[i], i});
-                res[i] = nums.size();
-            }
-            else{
-                res[i] = st.top().second;
-                st.push({nums[i], i});
-            }
+    vector<int> nse(vector<int>& heights){
+        vector<int> res(heights.size(), heights.size());
+        stack<int> st;
+        for(int i = heights.size()-1; i >= 0 ; i--){
+            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop(); 
+            if(!st.empty()) res[i] = st.top();
+            st.push(i);
         }
         return res;
     }
-    vector<int> pse(vector<int>& nums){
-        stack<pair<int, int>> st;
-        vector<int> res(nums.size());
-        for(int i = 0;i<nums.size();i++){
-            while(!st.empty()&&st.top().first>=nums[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                st.push({nums[i], i});
-                res[i] = -1;
-            }
-            else if(st.top().first<nums[i]){
-                res[i] = st.top().second;
-                st.push({nums[i], i});
-            }
+    vector<int> pse(vector<int>& heights){
+        vector<int> res(heights.size(), -1);
+        stack<int> st;
+        for(int i = 0;i<heights.size();i++){
+            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop(); 
+            if(!st.empty()) res[i] = st.top();
+            st.push(i);
         }
         return res;
     }
     int largestRectangleArea(vector<int>& heights) {
-        int maxArea = 0;
-        vector<int> psv = pse(heights);
-        vector<int> nsv = nse(heights);
+        vector<int> nsee = nse(heights);
+        vector<int> psee = pse(heights);
+        int maxarea = 0;
         for(int i = 0;i<heights.size();i++){
-            int psei = psv[i]+1;
-            int nsei = nsv[i]-1;
-            int area = (nsei-psei+1)*heights[i];
-            maxArea = max(area, maxArea);
+            int area = heights[i] * (nsee[i] - psee[i] - 1);
+            maxarea = max(area, maxarea);
         }
-        return maxArea;
+        return maxarea;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int maxArea = 0;
-        vector<int> heights(matrix[0].size(), 0);
+        vector<int> temp(matrix[0].size(), 0);
+        int maxarea = 0;
         for(int i = 0;i<matrix.size();i++){
             for(int j = 0;j<matrix[0].size();j++){
-                if(matrix[i][j]=='1'){
-                    heights[j]++;
-                }
-                else heights[j] = 0;
+                if(matrix[i][j] != '0') temp[j] += matrix[i][j] - '0'; 
+                else temp[j] = 0;
             }
-            int area = largestRectangleArea(heights);
-            maxArea = max(area, maxArea);
+            maxarea = max(maxarea, largestRectangleArea(temp));
         }
-        return maxArea;
+        return maxarea;
     }
 };
