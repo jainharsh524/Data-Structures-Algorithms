fe9cb6e7ebf3873089a1ac6ee514390860f1f07d
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-21 05:17:57 +0000

    [LeetCode] Runtime - 24 ms (52.71%), Memory - 139.1 MB (73.34%)

--- solutions/0239-sliding-window-maximum/solution.cpp
+++ solutions/0239-sliding-window-maximum/solution.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
         vector<int> res;
-        deque<int> dq; // stores INDICES
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            // remove out-of-window indices
-            if (!dq.empty() && dq.front() == i - k)
-                dq.pop_front();
-
-            // maintain decreasing order
-            while (!dq.empty() && nums[dq.back()] <= nums[i])
-                dq.pop_back();
-
-            dq.push_back(i);
-
-            // window formed
-            if (i >= k - 1)
-                res.push_back(nums[dq.front()]);
+        for(int i = 0;i<nums.size();i++){
+            if(q.front() == i-k) q.pop_front();
+            while(!q.empty() && nums[q.back()] <= nums[i]) q.pop_back();
+            q.push_back(i);
+            if(i >= k-1) res.push_back(nums[q.front()]);
         }
-
         return res;
     }
 };
-
