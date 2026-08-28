fb8f3490ed0c82ecfbd71a391031b34b3b7de300
jainharsh524 <harshnareshjain1729@gmail.com>
2026-08-28 07:02:58 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 21.7 MB (76.56%)

--- solutions/0057-insert-interval/solution.cpp
+++ solutions/0057-insert-interval/solution.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end(),
+        [](vector<int> &a, vector<int>& b){
+            return a[0]<b[0];
+        });
+        int start = intervals[0][0];
+        int end = intervals[0][1];
         vector<vector<int>> res;
-        int start = newInterval[0];
-        int end = newInterval[1];
-        int i = 0;
-        while(i<intervals.size()&&intervals[i][1]<start){
-            res.push_back(intervals[i]);
-            i++;
-        }
-        while(i<intervals.size()&&intervals[i][0]<=end){
-            start = min(intervals[i][0], start);
-            end = max(intervals[i][1], end);
-            i++;
+        for(int i= 1;i<intervals.size();i++){
+            int ns = intervals[i][0];
+            int ne = intervals[i][1];
+            if(end >= ns){
+                end = max(end, ne);
+                start = min(start, ns);
+            }
+            else{
+                res.push_back({start, end});
+                start = ns;
+                end = ne;
+            }
         }
         res.push_back({start, end});
-        while(i<intervals.size()){
-            res.push_back(intervals[i]);
-            i++;
-        }
         return res;
     }
 };
