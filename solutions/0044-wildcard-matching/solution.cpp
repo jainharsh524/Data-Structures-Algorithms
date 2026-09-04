c670bbac06a16dd8dbb19c4e3f474c02854b656c
jainharsh524 <harshnareshjain1729@gmail.com>
2026-09-04 09:44:07 +0000

    [LeetCode] Runtime - 147 ms (8.53%), Memory - 55.1 MB (6.22%)

--- solutions/0044-wildcard-matching/solution.cpp
+++ solutions/0044-wildcard-matching/solution.cpp
@@ -16,14 +16,15 @@ public:
             return dp[index1][index2] = match(s, p, index1 + 1, index2 + 1, dp);
         } else if (p[index2] == '*') {
             return dp[index1][index2] =
-                       match(s, p, index1 + 1, index2, dp) ||
-                       match(s, p, index1 + 1, index2 + 1, dp) ||
-                       match(s, p, index1, index2 + 1, dp);
+                       match(s, p, index1 + 1, index2, dp) || //* can be matched with multiple char
+                       match(s, p, index1 + 1, index2 + 1, dp) || //* can be matched with one char
+                       match(s, p, index1, index2 + 1, dp); //* is epsilon
         } else
             return dp[index1][index2] = false;
     }
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, -1));
+        vector<vector<int>> dp(s.size()+1, 
+        vector<int>(p.size()+1, -1));
         return match(s, p, 0, 0, dp);
     }
 };
