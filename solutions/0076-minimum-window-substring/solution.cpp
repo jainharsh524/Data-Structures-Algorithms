f02b304a1f042ceaf9f77301c1bab55ab25360f0
jainharsh524 <harshnareshjain1729@gmail.com>
2026-09-14 08:00:15 +0000

    [LeetCode] Runtime - 13 ms (39.94%), Memory - 11.4 MB (65.03%)

--- solutions/0076-minimum-window-substring/solution.cpp
+++ solutions/0076-minimum-window-substring/solution.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.size() < t.size()) return "";
-        unordered_map<char, int> freq;
-        for (char c : t) freq[c]++;
-        int required = t.size();
-        int left = 0;
-        int minLen = INT_MAX;
-        int start = 0;
-        for (int right = 0; right < s.size(); right++) {
-            if (freq[s[right]] > 0) required--;
-            freq[s[right]]--;
-            while (required == 0) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    start = left;
+        if(s == t) return s;
+        unordered_map<char, int> mp;
+        for(char ch: t) mp[ch]++;
+        int required = t.size(), minlen = INT_MAX, l = 0, start = -1;
+        for(int i = 0; i < s.size(); i++){
+            if(mp[s[i]] > 0) required--;
+            mp[s[i]]--;
+            
+            while(required == 0){
+                if(minlen > i - l + 1){
+                    minlen = min(minlen, i - l + 1);
+                    start = l;
                 }
-                freq[s[left]]++;
-                if (freq[s[left]] > 0) required++;
-                left++;
+                if(mp[s[l]] == 0) required++;
+                mp[s[l]]++;
+                
+                l++;
             }
         }
-        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+        return start == -1 ? "" : s.substr(start, minlen);
     }
 };
-
